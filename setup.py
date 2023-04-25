cb2ac7d33f4c5597e8ad1f745f7ad94d326ac5c2
shehanzsaleem <56260108+shehanzsaleem@users.noreply.github.com>
2023-04-25 17:43:27 +0530

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AutoNLP",
-    version="0.0.3",
+    version="0.0.4",
     description="TextForge AutoNLP - Automated Text Classification Tool",
     packages=find_packages(include=['AutoNLP', 'AutoNLP.*']),
     install_requires=[
